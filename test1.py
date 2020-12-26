e96a0f476729112a531d46afa300f336e6a7d11b
Satheesh-git <mkmsatheeshkumar@gmail.com>
2020-12-26 20:13:22 +0530

    comments

--- test1.py
+++ test1.py
@@ -1,5 +1,9 @@
 def hello():
     print("Hello world")
+<<<<<<< HEAD
     print("testing git")
     print("another file")
     print("commitcheck")
+=======
+    print("testing git")
+>>>>>>> parent of 6823276 (testing push)
