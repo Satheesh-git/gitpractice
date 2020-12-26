eccf13151b805e095ca64d1685458de4e4a12481
Satheesh-git <mkmsatheeshkumar@gmail.com>
2020-12-26 20:14:12 +0530

    Revert "comments"
    This reverts commit e96a0f476729112a531d46afa300f336e6a7d11b.

--- test1.py
+++ test1.py
@@ -1,9 +1,5 @@
 def hello():
     print("Hello world")
-<<<<<<< HEAD
     print("testing git")
     print("another file")
     print("commitcheck")
-=======
-    print("testing git")
->>>>>>> parent of 6823276 (testing push)
